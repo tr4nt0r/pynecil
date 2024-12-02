97285d4903c84705025cad82b9c58b51b98c43fe
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2024-12-02 19:22:50 +0100

    Bump version 1.0.1 → 2.0.0

--- pynecil/__init__.py
+++ pynecil/__init__.py
@@ -1,6 +1,6 @@
 """Pynecil - Python library to communicate with Pinecil V2 soldering irons via Bluetooth."""
 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 
 from .client import Pynecil, discover
 from .exceptions import CommunicationError
