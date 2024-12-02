222805e62bd9499df27b31abd42e41f92eda9a76
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2024-12-02 16:20:08 +0100

    Use enums for settings

--- pynecil/__init__.py
+++ pynecil/__init__.py
@@ -5,18 +5,29 @@ __version__ = "1.0.1"
 from .client import Pynecil, discover
 from .exceptions import CommunicationError
 from .types import (
+    AnimationSpeed,
+    AutostartMode,
+    BatteryType,
     CharBulk,
     CharLive,
     CharSetting,
     DeviceInfoResponse,
     LanguageCode,
     LiveDataResponse,
+    LockingMode,
+    LogoDuration,
     OperatingMode,
     PowerSource,
+    ScreenOrientationMode,
+    ScrollSpeed,
     SettingsDataResponse,
+    TempUnit,
 )
 
 __all__ = [
+    "AnimationSpeed",
+    "AutostartMode",
+    "BatteryType",
     "CharBulk",
     "CharLive",
     "CharSetting",
@@ -25,8 +36,13 @@ __all__ = [
     "discover",
     "LanguageCode",
     "LiveDataResponse",
+    "LockingMode",
+    "LogoDuration",
     "OperatingMode",
     "PowerSource",
     "Pynecil",
+    "ScreenOrientationMode",
+    "ScrollSpeed",
     "SettingsDataResponse",
+    "TempUnit",
 ]
